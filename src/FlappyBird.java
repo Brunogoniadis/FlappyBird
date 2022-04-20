2e550dfb4ca4959a713536449d6cf954fae9971f
Brunogoniadis <56612714+Brunogoniadis@users.noreply.github.com>
2022-04-19 22:18:34 -0300

    Adicionado Movimento no Ground
    Chão com movimentação infinita

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -1,5 +1,10 @@
 
 public class FlappyBird  implements Jogo{
+	
+	public double ground_offset = 0;
+	public double gvx = 70;
+	
+	
 	public String getTitulo() {
 		return "Flapy Bird Genérico";
 	}
@@ -10,6 +15,8 @@ public class FlappyBird  implements Jogo{
     	return 512;
     }
     public void tique(java.util.Set<String> teclas, double dt) {
+    	ground_offset +=dt*gvx; 
+    	ground_offset = ground_offset%308;
     	
     }
     
@@ -22,11 +29,14 @@ public class FlappyBird  implements Jogo{
     	//Background
     	tela.imagem("flappy.png", 0,0,288,512,0,0,0);
     	tela.imagem("flappy.png", 0,0,288,512,0,288,0);
-    	tela.imagem("flappy.png", 0,0,288,512,0,288*2,0);
+        //tela.imagem("flappy.png", 0,0,288,512,0,288*2,0);
     	
     	//Ground
-    	tela.imagem("flappy.png", 292, 0, 308, 112, 0, 0 , getAltura()-112);
-    	tela.imagem("flappy.png", 292, 0, 308, 112, 0, 308 , getAltura()-112);
+    	tela.imagem("flappy.png", 292, 0, 308, 112, 0, -ground_offset , getAltura()-112);
+    	tela.imagem("flappy.png", 292, 0, 308, 112, 0, 308 - ground_offset, getAltura()-112);
+    	tela.imagem("flappy.png", 292, 0, 308, 112, 0, 308 *2 -ground_offset , getAltura()-112);
+    	
+    	
     }
     
     public static void main (String[]args) {
